e43c21a4960c0f26cf991a40ead4a4fe3a7867fb
Alwi Akbar <alwiakbar09@gmail.com>
2025-09-05 16:40:04 +0700

    feat:update cart route

--- src/routes/cart.router.ts
+++ src/routes/cart.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { jwtVerify } from "../middlewares/jwt.verify";
-import { addtoCartController } from "../controllers/cart.controller";
+import { addtoCartController, updateCartItemController } from "../controllers/cart.controller";
 
 const cartRouter = Router ()
 
 cartRouter.post("/items", jwtVerify, addtoCartController);
-cartRouter.post("/items", jwtVerify, addtoCartController);
+cartRouter.post("/items/:itemId", jwtVerify, updateCartItemController);
 
 export default cartRouter;

dee911cce807568fcb939e8e6ee6eca28a4cbbf7
Alwi Akbar <alwiakbar09@gmail.com>
2025-09-05 22:59:22 +0700

    complete cart API

--- src/routes/cart.router.ts
+++ src/routes/cart.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { jwtVerify } from "../middlewares/jwt.verify";
-import { addtoCartController, updateCartItemController } from "../controllers/cart.controller";
+import { addtoCartController, deleteCartController, getCartItemsController, updateCartItemController } from "../controllers/cart.controller";
 
 const cartRouter = Router ()
 
-cartRouter.post("/items", jwtVerify, addtoCartController);
-cartRouter.post("/items/:itemId", jwtVerify, updateCartItemController);
+cartRouter.post("/items", jwtVerify,  addtoCartController);
+cartRouter.patch("/items/:itemId", jwtVerify,  updateCartItemController);
+cartRouter.delete("/items/:itemId", jwtVerify, deleteCartController);
+cartRouter.get("/items",jwtVerify,  getCartItemsController);
 
 export default cartRouter;
--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -95,3 +95,51 @@ export const updateCartItemService = async ({
         }
     })
 }
+
+export const deleteCartService = async ({
+    userId,
+    itemId
+}: {userId: string; itemId:string}) => {
+    const cart = await prisma.shoppingCart.findFirst({
+        where: {userId, isActive:true},
+        include : {
+            ShoppingCartItem: {
+                where: {id : itemId}
+            }
+        }
+    });
+
+    if (!cart) {
+        throw {message: "Active cart not found", isExpose: true}
+    }
+
+    const existingItem = cart.ShoppingCartItem[0];
+    if (!existingItem) {
+    throw { message: "Item not found in cart", isExpose: true };
+    }
+
+    await prisma.shoppingCartItem.delete({
+        where: {id: existingItem.id}
+    });
+
+    return {message: "Item removed from cart successfully" }
+}
+
+export const getCartItemsService = async (userId: string) => {
+  const cart = await prisma.shoppingCart.findFirst({
+    where: { userId, isActive: true },
+    include: {
+      ShoppingCartItem: {
+        include: {
+          product: { select: { name: true, price: true } },
+        },
+      },
+    },
+  });
+
+  if (!cart) {
+    return { message: "Cart is empty", items: [] };
+  }
+
+  return { items: cart.ShoppingCartItem };
+};

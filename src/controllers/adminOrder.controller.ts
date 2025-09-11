6c5ea67c9c608d4f33847aaec74ede72499770ba
Alwi Akbar <alwiakbar09@gmail.com>
2025-09-11 10:54:17 +0700

    get detail order by admin

--- src/controllers/adminOrder.controller.ts
+++ src/controllers/adminOrder.controller.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from "express";
-import { getAllOrdersAdminService } from "../services/adminOrder.service";
+import { getAllOrdersAdminService, getOrderDetailAdminService } from "../services/adminOrder.service";
 import { formatDateJakarta } from "../utils/date";
+import { UserRole } from "../generated/prisma";
 
 export const getAllOrdersAdminController = async (req: Request, res: Response) => {
     
-  const {userId} = res.locals.payload
+  const { user_id, role, storeId } = res.locals.payload;
+  console.log("DEBUG JWT Payload:");
+  console.log("user_id:", user_id);
+  console.log("role:", role);
+  console.log("storeId:", storeId);
 
-  const orders = await getAllOrdersAdminService();
+  if (role !== UserRole.SUPER_ADMIN && role !== UserRole.ADMIN_STORE) {
+    throw { status: 403, message: "Forbidden: You are not authorized to access this resource" };
+  }
+
+  const orders = await getAllOrdersAdminService({user_id, role, storeId});
 
   const mappedOrders = orders.map((order) => ({
     orderId: order.id,
@@ -38,3 +47,51 @@ export const getAllOrdersAdminController = async (req: Request, res: Response) =
     data: mappedOrders
   });
 };
+
+export const getOrderDetailController =  async (req: Request, res: Response) => {
+  const { role, storeId} = res.locals.payload
+  const { orderId } = req.params;
+
+  console.log("DEBUG JWT Payload:");
+  console.log("role:", role);
+  console.log("storeId:", storeId);
+
+  if (role !== UserRole.SUPER_ADMIN && role !== UserRole.ADMIN_STORE) {
+      throw { status: 403, message: "Forbidden: You are not authorized to access this resource" };
+    }
+
+  const order = await getOrderDetailAdminService({role, storeId, orderId});
+
+  if (!order) {
+    throw { status: 404, message: "Order not found or not accessible" };
+  }
+
+  const mappedOrder = {
+    orderId: order.id,
+    status: order.status,
+    createdAt: formatDateJakarta(order.createdAt),
+    totalPrice: order.totalPrice,
+    discount: order.discount,
+    finalPrice: order.finalPrice,
+    paymentProof: order.paymentProof,
+    customer: {
+      id: order.user.id,
+      fullName: order.user.fullName,
+      email: order.user.email,
+      phoneNumber: order.user.phoneNumber,
+      addresses: order.user.UserAddress.map((a) => a.address),
+    },
+    items: order.OrderItems.map((item) => ({
+      productId: item.productId,
+      name: item.product.name,
+      price: item.price,
+      quantity: item.quantity,
+      subTotal: item.subTotal,
+    })),
+  };
+
+  return res.status(200).json({
+    message: `Get order: ${orderId} detail sucessfully`,
+    data: mappedOrder
+  });
+};  
--- src/routes/orderAdmin.router.ts
+++ src/routes/orderAdmin.router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { jwtVerify } from "../middlewares/jwt.verify";
-import { getAllOrdersAdminController } from "../controllers/adminOrder.controller";
+import { getAllOrdersAdminController, getOrderDetailController } from "../controllers/adminOrder.controller";
 import { ensureRole, ensureStoreOwnership } from "../middlewares/role.middleware";
 import { roleVerify } from "../middlewares/role.verify";
 
 const orderAdminRouter = Router ()
 
-orderAdminRouter.get("/orders", jwtVerify, roleVerify(["SUPER_ADMIN"]), getAllOrdersAdminController);
+orderAdminRouter.get("/orders", jwtVerify, roleVerify(["SUPER_ADMIN","ADMIN_STORE" ]), getAllOrdersAdminController);
+orderAdminRouter.get("/orders/:orderId", jwtVerify, roleVerify(["SUPER_ADMIN","ADMIN_STORE" ]), getOrderDetailController);
+
 
 export default orderAdminRouter;

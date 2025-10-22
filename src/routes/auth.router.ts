710fcf315c03a7b6a83a38501798439aa9f85ec1
Fauzan <fauzan.c4@gmail.com>
2025-10-23 01:42:49 +0700

    feat: add validate all request endpoint auth

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -17,19 +17,58 @@ import {
 } from "../controllers/auth.controller";
 import { jwtVerify } from "../middlewares/jwt.verify";
 import limiter from "../middlewares/rate.limiter";
+import { validateYup } from "../middlewares/validateYup";
+import {
+  authChangeEmailSchema,
+  authChangePasswordSchema,
+  authLoginSchema,
+  authRegisterSchema,
+  authRequestResetPasswordSchema,
+  authResendRegisterVerificationSchema,
+  authResetPasswordSchema,
+  authVerificationEmailSchema,
+} from "../validations/auth.validation";
 
 const authRouter = Router();
 
-authRouter.post("/register", authRegisterController);
-authRouter.post("/verify-email", jwtVerify, authVerificationEmailController);
-authRouter.post("/login", authLoginController);
-authRouter.post("/request-reset-password", authRequestResetPasswordController);
-authRouter.post("/reset-password", jwtVerify, authResetPasswordController);
-authRouter.post("/change-password", jwtVerify, authChangePasswordController);
+authRouter.post(
+  "/register",
+  validateYup(authRegisterSchema),
+  authRegisterController
+);
+authRouter.post(
+  "/verify-email",
+  jwtVerify,
+  validateYup(authVerificationEmailSchema),
+  authVerificationEmailController
+);
+authRouter.post("/login", validateYup(authLoginSchema), authLoginController);
+authRouter.post(
+  "/request-reset-password",
+  validateYup(authRequestResetPasswordSchema),
+  authRequestResetPasswordController
+);
+authRouter.post(
+  "/reset-password",
+  jwtVerify,
+  validateYup(authResetPasswordSchema),
+  authResetPasswordController
+);
+authRouter.post(
+  "/change-password",
+  jwtVerify,
+  validateYup(authChangePasswordSchema),
+  authChangePasswordController
+);
 authRouter.get("/session", jwtVerify, authSessionLoginController);
 authRouter.get("/google", authGoogleController);
 authRouter.get("/google/callback", authGoogleCallbackController);
-authRouter.post("/email/change", jwtVerify, authChangeEmailController);
+authRouter.post(
+  "/email/change",
+  jwtVerify,
+  validateYup(authChangeEmailSchema),
+  authChangeEmailController
+);
 authRouter.post(
   "/email/verify-email",
   jwtVerify,
@@ -38,6 +77,7 @@ authRouter.post(
 authRouter.post(
   "/register/resend-verification",
   limiter,
+  validateYup(authResendRegisterVerificationSchema),
   authResendRegisterVerificationController
 );
 authRouter.post(

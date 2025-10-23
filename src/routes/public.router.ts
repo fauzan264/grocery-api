00247ad8d4d253ac35271474d7ecdcf753c71cf8
Fauzan <fauzan.c4@gmail.com>
2025-10-24 05:08:15 +0700

    feat: create endpoint public list and detail product

--- src/routes/public.router.ts
+++ src/routes/public.router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import {
+  getPublicProductByIDController,
+  getPublicProductsController,
   getPublicStoreByIdController,
   getPublicStoreNearbyController,
 } from "../controllers/public.controller";
@@ -8,5 +10,7 @@ const publicRouter = Router();
 
 publicRouter.get("/stores/nearby", getPublicStoreNearbyController);
 publicRouter.get("/stores/:id", getPublicStoreByIdController);
+publicRouter.get("/products", getPublicProductsController);
+publicRouter.get("/products/:id", getPublicProductByIDController);
 
 export default publicRouter;
